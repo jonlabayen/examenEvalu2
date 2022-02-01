9dc0bab9ee3c93e7ab3d0231253e97230ea0fa76
Ekaitz10 <esotoap19dw31@ikzubirimanteo.com>
2022-02-01 09:07:29 +0100

    tabla pivot creada

--- database/migrations/2021_01_31_160212_create_flights_table.php
+++ database/migrations/2021_01_31_160212_create_flights_table.php
@@ -14,7 +14,7 @@ class CreateFlightsTable extends Migration
     public function up()
     {
         Schema::create('flights', function (Blueprint $table) {
-            $table->id();
+           $table->increments('id');
             $table->string("name");
             $table->date("date");
             $table->string("origin");
